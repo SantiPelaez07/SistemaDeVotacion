d2759ee9825b3863bdc7be153c4dbc8a75ef3f96
SantiPelaez07 <santipelaez0@gmail.com>
2025-02-16 19:49:36 -0500

    Implementation of the necessary methods to make the voting endpoints work.

---
+++ src/models/VoteModel.js
@@ -0,0 +1,99 @@
+const db = require("../config/database");
+const VoteRequest = require("../dtos/request/VoteRequest");
+
+async function createVote(req) {
+    try {
+        const rq = new VoteRequest(req);
+        console.log("Este es el request del model: ", rq);
+        const [candidate] = await db.execute(
+            "SELECT * FROM candidate WHERE id = ?",
+            [rq.candidate_id]
+        );
+
+        const [voter] = await db.execute(
+            "SELECT * FROM voter WHERE id = ?",
+            [rq.voter_id]
+        );
+
+        if(candidate.length === 0){
+            throw new Error(`The candidate with the ID: ${rq.candidate_id} does not exist in the BD.`);
+        }
+
+        if(voter.length === 0){
+            throw new Error(`The voter with the ID: ${rq.voter_id}, does not exist in the DB.`);
+        }
+
+        const result = await db.execute(
+            "INSERT INTO vote (voter_id, candidate_id) VALUES (?,?)",
+            [rq.voter_id, rq.candidate_id]
+        );
+
+        await db.execute(
+            "UPDATE candidate SET votes = votes + 1 WHERE id = ?",
+            [rq.candidate_id]
+        );
+
+        if(result[0].insertId == undefined){
+            throw new error("Error getting created id: ", error);
+        }
+        const vote = await getVoteById(result[0].insertId);
+        return vote;
+    } catch (error) {
+        console.error("An error occurred: ", error);
+        throw error;
+    }
+}
+
+
+async function getAllVotes() {
+    const votes = await db.execute(
+        "SELECT * FROM vote"
+    );
+    return [votes];
+}
+
+async function getVoteById(id) {
+    const [vote] = await db.execute(
+        "SELECT * FROM vote WHERE id = ?",
+        [id]
+    );
+    return vote.length > 0 ? vote[0] : null;
+    
+}
+
+async function getstatistics() {
+    const [candidates] = await db.execute(
+        "SELECT * FROM candidate",
+
+    );
+    console.log("Estos son los candidatos desde el model: ", candidates);
+
+    const [totalVotersResult] = await db.execute(
+        "SELECT COUNT(*) AS nv FROM vote"
+    )
+    const totalVoters = totalVotersResult[0].nv;
+    const formatter = new Intl.NumberFormat("en-US", {
+        style: 'percent',
+        maximumFractionDigits: 0
+    });
+
+    const newStatistics = [];
+
+    candidates.forEach(candidate => {
+        const percentage = totalVoters > 0 ? candidate.votes / totalVoters : 0;
+        const statistics = {
+            id: candidate.id,
+            name_candidate: candidate.name,
+            total_votes: candidate.votes,
+            percentage_votes: formatter.format(percentage),
+            total_voters: totalVoters
+        };
+        newStatistics.push(statistics);
+    });
+  
+console.log("Lista nueva del model despues de recorer: ", newStatistics)
+
+    return newStatistics;
+}
+
+module.exports = { createVote, getAllVotes, getstatistics };
